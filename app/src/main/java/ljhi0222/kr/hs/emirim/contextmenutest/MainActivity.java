ec46f82cbbe22409b04eb09de7a871d2e0c0fa8f
lej3802 <daylock3802@gmail.com>
2017-05-25 19:30:21 +0900

    #4 Event Programming

--- app/src/main/java/ljhi0222/kr/hs/emirim/contextmenutest/MainActivity.java
+++ app/src/main/java/ljhi0222/kr/hs/emirim/contextmenutest/MainActivity.java
@@ -1,20 +1,29 @@
 package ljhi0222.kr.hs.emirim.contextmenutest;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.ContextMenu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+    LinearLayout linear;
+    Button but2;
+    ImageView img1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button but1=(Button)findViewById(R.id.but1);
-        Button but2=(Button)findViewById(R.id.but2);
+        but2=(Button)findViewById(R.id.but2);
+        linear = (LinearLayout)findViewById(R.id.linear);
+        img1 = (ImageView)findViewById(R.id.img1);
         registerForContextMenu(but1);
         registerForContextMenu(but2);
     }
@@ -25,12 +34,37 @@ public class MainActivity extends AppCompatActivity {
         MenuInflater menuInflater=getMenuInflater();
         if(v.getId() == R.id.but1){
             menu.setHeaderTitle("배경색 변경");
-                menu.setHeaderIcon(R.drawable.changwook);
+                menu.setHeaderIcon(R.drawable.changwook2);
             menuInflater.inflate(R.menu.menu1, menu);
         }
 
         if(v.getId() == R.id.but2){
             menuInflater.inflate(R.menu.menu2, menu);
         }
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        super.onContextItemSelected(item);
+        switch(item.getItemId()){
+            case R.id.menu1_item_red:
+                linear.setBackgroundColor(Color.RED);
+                return true;
+            case R.id.menu1_item_blue:
+                linear.setBackgroundColor(Color.BLUE);
+                return true;
+            case R.id.menu1_item_green:
+                linear.setBackgroundColor(Color.GREEN);
+                return true;
+            case R.id.menu2_item_rotation:
+                img1.setRotation(img1.getRotation()+45);
+                return true;
+            case R.id.menu2_item_size:
+                img1.setScaleX(img1.getScaleX()*1.5f);
+                img1.setScaleY(img1.getScaleY()*1.5f);
+                return true;
+        }
+
+        return false;
     }
 }
